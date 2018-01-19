a570c4f900b20f46477725d4a47ddc1a0519cf49
Ikeda <brandon_ikeda@hotmail.com>
2018-01-19 07:06:45 -0800

    Finished Firebase

--- App.js
+++ App.js
@@ -8,8 +8,21 @@ import {
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducers from './src/reducers';
+import firebase from 'firebase';
 
 export default class App extends Component<{}> {
+  componentWillMount() {
+    var config = {
+      apiKey: 'AIzaSyADHoLfmYylUNZTYUXI35M8d7oR_OQ_0TM',
+      authDomain: 'megamanager-34f29.firebaseapp.com',
+      databaseURL: 'https://megamanager-34f29.firebaseio.com',
+      projectId: 'megamanager-34f29',
+      storageBucket: 'megamanager-34f29.appspot.com',
+      messagingSenderId: '720382922395'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
