92e18a29c0fc1da82c6d8db98a1059bf82e3ae7c
Ikeda <brandon_ikeda@hotmail.com>
2018-02-24 23:08:50 +1300

    Added right title

--- src/Router.js
+++ src/Router.js
@@ -11,7 +11,7 @@ const RouterComponent = () => {
           <Scene key="login" component={LoginForm} title="Please Login" />
         </Scene>
         <Scene key="main">
-          <Scene key="employeeList" component={EmployeeList} title="Employees" />
+          <Scene rightTitle="Add" onRight={() => {console.log('right')}} key="employeeList" component={EmployeeList} title="Employees" />
         </Scene>
       </Scene>
     </Router>
