10e5e05d2a383abe42abb194af52cae1bb7cbf84
Ikeda <brandon_ikeda@hotmail.com>
2018-02-26 16:43:05 -0800

    135

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import {Card, CardSection, Input, Button} from './common';
 
 class EmployeeCreate extends Component {
   render (){
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+          label="Name"
+          placeholder="John Smith"
+          />
+        </CardSection>
+        <CardSection>
+          <Input
+          label="Phone"
+          placeholder="123-456-7890"
+          />
+        </CardSection>
+        <CardSection>
+        </CardSection>
+      </Card>
     );
   }
 }
