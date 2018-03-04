c22db5cc84cb04bf02283baf5f38e5d43552c810
Ikeda <brandon_ikeda@hotmail.com>
2018-03-03 23:20:19 -0800

    138

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Picker} from 'react-native';
 import {connect} from 'react-redux';
 import {employeeUpdate} from '../actions';
 import {Card, CardSection, Input, Button} from './common';
@@ -22,6 +23,21 @@ class EmployeeCreate extends Component {
           value ={this.props.phone}
           onChangeText={text => this.props.employeeUpdate({prop: 'phone', value: text})}
           />
+        </CardSection>
+        <CardSection>
+          <Picker
+            style={{flex: 1}}
+            selectedValue={this.props.shift}
+            onValueChange={day => this.props.employeeUpdaye({prop: 'shift', value})}
+            >
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
         </CardSection>
         <CardSection>
         </CardSection>
