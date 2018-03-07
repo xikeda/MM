fb5e1aa2d58f0f0cc19de6a123695cfbf20c0f93
Ikeda <brandon_ikeda@hotmail.com>
2018-03-06 20:21:45 -0800

    142

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 import {Picker, Text} from 'react-native';
 import {connect} from 'react-redux';
-import {employeeUpdate} from '../actions';
+import {employeeUpdate, employeeCreate} from '../actions';
 import {Card, CardSection, Input, Button} from './common';
 
 class EmployeeCreate extends Component {
+  onButtonPress(){
+    const {name, phone, shift} = this.props;
+
+    this.props.employeeCreate({name, phone, shift});
+  }
+
   render (){
     return (
       <Card>
@@ -12,7 +18,7 @@ class EmployeeCreate extends Component {
         <CardSection>
           <Input
           label="Name"
-          placeholder="John Smith"
+          placeholder="Ferris Bueller"
           value={this.props.name}
           onChangeText={text => this.props.employeeUpdate({prop: 'name', value: text})}
           />
@@ -42,6 +48,12 @@ class EmployeeCreate extends Component {
             <Picker.Item label="Sunday" value="Sunday" />
           </Picker>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onButtonPress.bind(this)}>
+            Create
+          </Button>
+        </CardSection>
       </Card>
     );
   }
@@ -60,4 +72,4 @@ const mapStateToProps = (state) => {
   return {name, phone, shift};
 };
 
-export default connect(mapStateToProps, {employeeUpdate}) (EmployeeCreate);
+export default connect(mapStateToProps, {employeeUpdate, employeeCreate}) (EmployeeCreate);
