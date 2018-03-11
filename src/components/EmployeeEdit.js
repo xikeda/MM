134eb7907527e8a883acf229a275fff7b5996434
Ikeda <brandon_ikeda@hotmail.com>
2018-03-10 19:33:08 -0800

    161

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Communications from 'react-native-communications';
 import {employeeUpdate, employeeSave} from '../actions';
-import {Card, CardSection, Button} from './common';
+import {Card, CardSection, Button, Confirm} from './common';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
+  state = {showModal: false};
+
   componentWillMount(){
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({prop, value});
@@ -18,6 +21,12 @@ class EmployeeEdit extends Component {
     this.props.employeeSave({name, phone, shift, uid: this.props.employee.uid});
   }
 
+  onTextPress(){
+    const {phone, shift} = this.props;
+
+    Communications.text(phone, `Your upcoming shift is on ${shift}`);
+  }
+
   render (){
     return (
       <Card>
@@ -26,10 +35,21 @@ class EmployeeEdit extends Component {
           <Button onPress={this.onButtonPress.bind(this)}>
             Save Changes
           </Button>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Delete
+        </CardSection>
+        <CardSection>
+          <Button onPress={this.onTextPress.bind(this)}>
+            Text Schedule
           </Button>
         </CardSection>
+        <CardSection>
+          <Button onPress={() => this.setState({showModal: !this.state.showModal})}>
+            Fire Employee
+          </Button>
+        </CardSection>
+        <Confirm
+        visible={this.state.showModal}>
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     );
   }
